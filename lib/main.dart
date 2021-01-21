87db11168641cf53fbc6d5db22f87c2a102aacce
David <davidpineda.co@gmail.com>
2021-01-21 14:30:31 +0000

    use image from assets on the body

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child: Text("I Am Rich"),
-            ),
-          ),
-          body: Center(
-            child: Text("Gem"),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("I am Rich"),
           ),
         ),
+        body: Center(
+          child: Image(image: AssetImage('images/gem.png'),),
+        ),
       ),
-    );
+    ),
+  );
+}
