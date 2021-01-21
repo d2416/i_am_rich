b04c7df6fa6e3917f1266a9d6e2924a2ec0ae0ab
David <davidpineda.co@gmail.com>
2021-01-21 13:43:06 +0000

    Improve indentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text("Hola mundo")));
+void main() => runApp(
+      MaterialApp(
+        home: Text("Hola mundo"),
+      ),
+    );
